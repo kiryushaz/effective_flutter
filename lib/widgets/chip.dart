93dbc60c33c17cb7a984e59086153250e2696f63
Kirill Zykov <kiryushaz.gaming@gmail.com>
2023-11-30 14:59:51 +0600

    при нажатии на элемент чипса меняется цвет (выделение)

--- lib/widgets/chip.dart
+++ lib/widgets/chip.dart
@@ -20,15 +20,50 @@ class SberCards extends StatelessWidget {
           spacing: 8,
           runSpacing: 8,
           children: <Widget>[
-            for (String item in items) 
-              Chip(label: Text(item, style: const TextStyle(
-                fontWeight: FontWeight.w500, 
-                fontSize: 14, 
-                letterSpacing: -0.40
-              )))
+            for (String item in items)
+              SberCardItem(item: item)
           ],
         )
       ],
+    );
+  }
+}
+
+class SberCardItem extends StatefulWidget {
+  final String item;
+  const SberCardItem({super.key, required this.item});
+
+  @override
+  State<StatefulWidget> createState() => _SberCardItemState();
+}
+
+class _SberCardItemState extends State<SberCardItem> {
+  bool _selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          _selected = !_selected;
+        });
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 7),
+        decoration: ShapeDecoration(
+          color: _selected ? Colors.green.withOpacity(0.55) : Colors.black.withOpacity(0.08),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+        child: Text(widget.item, 
+          style: const TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 14,
+            letterSpacing: -0.4
+          ),
+        )
+      )
     );
   }
 }
